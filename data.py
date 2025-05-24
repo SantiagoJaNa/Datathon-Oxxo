287926c18f0c4665070dd27b1e75ac8857420957
Agustina01769867 <a01769867@itesm.mx>
2025-05-24 13:17:49 -0600

    Duplicates

--- data.py
+++ data.py
@@ -1,1 +1,14 @@
-with csv open
+import csv
+import pandas as pd
+
+# Read CSV
+with open('data.csv', 'r') as file:
+    reader = csv.reader(file)
+    data = [row for row in reader]
+
+    import pandas as pd
+
+df = pd.read_csv('')
+df = df.drop_duplicates(keep='first')
+
+df.to_csv('cleaned_file.csv', index=False)
